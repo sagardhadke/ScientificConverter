a54e63aa213466367be48461e51828d18cda3b60
Fashionuniqueworld <priyadhadke2009@gmail.com>
2021-11-23 20:05:50 +0530

    test5

--- app/src/main/java/net/uniquecomputer/bcaallinone/MainActivity.java
+++ app/src/main/java/net/uniquecomputer/bcaallinone/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,10 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnCalculator);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CalculatorApi.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         bottomNavigationView = findViewById(R.id.botton_navigator);
         bottomNavigationView.setSelectedItemId(R.id.home);
