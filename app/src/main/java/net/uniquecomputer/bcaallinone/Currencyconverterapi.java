3bc2ad40d396d8343788ecbddebe3ca661615113
Fashionuniqueworld <peaceunique2003@gmail.com>
2021-11-27 19:19:32 +0530

    test17

--- app/src/main/java/net/uniquecomputer/bcaallinone/Currencyconverterapi.java
+++ app/src/main/java/net/uniquecomputer/bcaallinone/Currencyconverterapi.java
@@ -2,13 +2,42 @@ package net.uniquecomputer.bcaallinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Currencyconverterapi extends AppCompatActivity {
-
+    private WebView mywebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currencyconverterapi);
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://sagardhadke.github.io/Currency-Converter/");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    public class mywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(mywebView.canGoBack()) {
+            mywebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
