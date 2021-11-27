315063f636ee556d2286cae69f9663f978b9656f
Fashionuniqueworld <peaceunique2003@gmail.com>
2021-11-27 17:22:02 +0530

    test14

--- app/src/main/java/net/uniquecomputer/bcaallinone/dataconverterapi.java
+++ app/src/main/java/net/uniquecomputer/bcaallinone/dataconverterapi.java
@@ -2,7 +2,15 @@ package net.uniquecomputer.bcaallinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class dataconverterapi extends AppCompatActivity {
 
@@ -10,5 +18,43 @@ public class dataconverterapi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataconverterapi);
+
+        ListView listView = findViewById(R.id.listviewdata);
+
+        List<String> list = new ArrayList<>();
+        list.add("KB-MB");
+        list.add("MB-KB");
+        list.add("MB-GB");
+        list.add("GB-MB");
+        list.add("MB-TB");
+        list.add("TB-MB");
+        list.add("GB-TB");
+        list.add("TB-GB");
+        list.add("Bytes-MB");
+        list.add("Bytes-GB");
+        list.add("Bytes-TB");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    //KB-MB
+                    startActivity(new Intent(dataconverterapi.this,KBMB.class));
+
+                }else if (position==1){
+                    //MB-KB
+                    startActivity(new Intent(dataconverterapi.this,MBKB.class));
+
+                }else if (position==2){
+
+
+                }
+
+            }
+        });
+
     }
 }
