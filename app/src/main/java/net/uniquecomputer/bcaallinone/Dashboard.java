e8d499bea18af829e9ded0c9a9ba5325f80cf249
Fashionuniqueworld <peaceunique2003@gmail.com>
2021-11-29 14:35:34 +0530

    test29

--- app/src/main/java/net/uniquecomputer/bcaallinone/Dashboard.java
+++ app/src/main/java/net/uniquecomputer/bcaallinone/Dashboard.java
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    ImageView imageView5;   // creating object
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        imageView5=findViewById(R.id.imageView5);     // giving id to the image
+        imageView5.setOnClickListener(new View.OnClickListener() {                     // click of image
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this,CategoryActivity.class));         // calling another  activity
+            }
+        });
 
         bottomNavigationView = findViewById(R.id.botton_navigator);
         bottomNavigationView.setSelectedItemId(R.id.dashboard);
